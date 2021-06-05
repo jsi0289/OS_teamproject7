59a99e2d341ad31cefb6416bce0311db6be92388
sunjh96 <35713465+sunjh96@users.noreply.github.com>
2021-06-05 17:51:23 +0900

    #Elasticsearch 검색기능 추가

--- News_DAUM.py
+++ News_DAUM.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-import re
 import requests
+import json
+import pprint
 from bs4 import BeautifulSoup
 from konlpy.tag import Okt
 from collections import Counter
+from elasticsearch import Elasticsearch
 
 def get_tags(text, ntags=50):
     spliter = Okt()
-    nouns = spliter.nouns(text)  
-    nouns1 = [n for n in nouns if len(n) > 1]   
+    nouns = spliter.nouns(text)
+    nouns1 = [n for n in nouns if len(n) > 1]
     count = Counter(nouns1)
     return_list = []
 
@@ -20,24 +22,19 @@ def get_tags(text, ntags=50):
 
     return return_list
 
-def replace(text):
-    sym = ['[',',','.',']',':','/', '^','|','&','*','-','–','%','=', '(',')','"',"'"]
-    text = re.sub('','',text,0).strip()
 
-    for i in sym:
-       text =  text.replace(i,'')
-
-    print(text)
-
-def crawler(category):
+def crawler(category, date):
     list = []
-    page = 1  
+    data = []
+    page = 1
+    count_news = 0   
+    num = 0   
 
     while True:
-        url =  'https://news.daum.net/breakingnews/' + category +'?page=' + str(page)
+        url = 'https://news.daum.net/breakingnews/' + category + \
+            '?page=' + str(page) + '&regDate=' + str(date)
         res = requests.get(url, headers=headers).text
-        soup = BeautifulSoup(res, 'html.parser')      
-        page += 1
+        soup = BeautifulSoup(res, 'html.parser')
 
         title = soup.select('ul > li > div > strong > a')
         lastpage_check = soup.select('p')
@@ -45,45 +42,94 @@ def crawler(category):
         if len(lastpage_check) == 1:
             break
 
-        for i in range(len(title)):
-            #replace(title[i].text)
-            list.append(title[i].text)      
+        for i in range(len(title)):           
+            count_news += 1
+            data.append({
+                '1.Platform': "다음",
+                '2.Category': category + '뉴스',
+                '3.Title': title[i].text,               
+                '4.Url': title[i]['href'],
+                '5.Date': date,
+                '6.Number': count_news
+            })
+            list.append(title[i].text)
 
-    tags = get_tags(' '.join(list), 10) 
+        page += 1
+
+    tags = get_tags(' '.join(list), 10)
 
     for tag in tags:
-       noun = tag['tag']
-       count = tag['count']
-       print('{} {}'.format(noun, count))
+        num += 1
+        noun = tag['tag']
+        count = tag['count']
+        print(f'{num}.' + '{} {}'.format(noun, count))
 
-    print('총 페이지 : ' + str(page))
+    with open('Data[' + category + '].json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent='\t', ensure_ascii=False)
 
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'}
+    print('총 페이지 : ' + str(page - 1))
 
-#Society
-print("\nSociety News") 
-crawler('society')
+def check_index():
+   return es.indices.get_alias("*")
 
-#Politics
-print("\nPolitics News") 
-crawler('politics')
+def create_index(mapping_json):	
+	if not es.indices.exists(index=index):
+		with open(mapping_json, 'r', encoding='utf-8') as f:
+			mapping = json.load(f)
+		res = es.indices.create(index=index, body = mapping)
+		
+		return res
 
-#Economic
-print("\nEconomic News") 
-crawler('economic')
+def delete_index():
+	if es.indices.exists(index=index):
+		return es.indices.delete(index=index)
 
-#Foreign
-print("\nForeign News") 
-crawler('foreign')
+def insert(doc_json):
+	with open(doc_json, 'r', encoding='utf-8') as f:
+		json_data = json.load(f)
+	
+	for i in json_data:
+		res = es.index(index=index, body=i)
+	
+	return res
 
-#Culture
-print("\nCulture News") 
-crawler('culture')
+def search(keyword=None):
+	body = {
+		"query": {
+			"match": {
+				"3.Title" : keyword				
+				}
+		}
+	}
+	res = es.search(index=index, body=body, request_timeout= 30)
 
-#Entertain
-print("\nEntertain News") 
-crawler('entertain')
+	return res
 
-#IT
-print("\nIT News") 
-crawler('digital')
+if __name__ == '__main__':
+   headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'}
+   url_elastic = '127.0.0.1'
+   port = '9200'
+   index = 'news'
+   mapping_json = 'mapping_nori.json'
+   categories = ['society', 'politics', 'economic', 'foreign', 'culture', 'entertain', 'digital']
+   date = 20210605
+   
+   es = Elasticsearch(f'{url_elastic}:{port}')
+
+   #for category in categories:
+      #print('\n'+ category + ' 뉴스')
+      #crawler(category, date)
+        
+   #delete_index()
+   #create_index(mapping_json)
+
+   #for category in categories:
+      #insert('Data[' + category + '].json')
+
+   r = search("코로나")
+   pprint.pprint(r)
+
+
+
+
+
